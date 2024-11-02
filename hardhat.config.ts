4780417c932c2d78bf6dd809056c4566d6eac7f1
yashm001 <yashmandil@gmail.com>
2024-11-03 01:48:56 +0530

    added deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,57 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: '0.8.24',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    baseSepolia: {
+      url: `https://sepolia.base.org`,
+      accounts: [...((process.env.PRIVATE_KEY?.split(',') as string[]) || '')],
+      chainId: 84532,
+    },
+    baseMainnet: {
+      url: `https://mainnet.base.org`,
+      accounts: [...((process.env.PRIVATE_KEY?.split(',') as string[]) || '')],
+      chainId: 8453,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      baseSepolia: process.env.BASESCAN_API_KEY,
+      baseMainnet: process.env.BASESCAN_API_KEY
+    },
+    customChains: [
+      {
+          network: "baseSepolia",
+          chainId: 84532,
+          urls: {
+              apiURL: "https://api-sepolia.basescan.org/api",
+              browserURL: "https://sepolia.basescan.org/",
+          },
+      },
+      {
+        network: "baseMainnet",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org/",
+        },
+      },
+  ],
+  },
 };
 
 export default config;
---
+++ ignition/modules/deploy.ts
@@ -0,0 +1,12 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const DeployModule = buildModule("DeployModule", (m) => {
+    const owner = m.getAccount(0);
+    // console.log("owner", owner);
+    const signerAddress = m.getAccount(1);
+    const tUSDC = m.contract("tUSDC");
+    const campaignRegistry = m.contract("CampaignRegistry",[owner, signerAddress]);
+    return { campaignRegistry, owner, signerAddress, tUSDC };
+});
+
+export default DeployModule;
